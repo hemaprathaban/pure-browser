3c21fc53afdb1b35ff20580e806650a301e04701
Mike Hommey <glandium@debian.org>
2007-12-08 19:24:40 +0100

    Set javascript.options.showInConsole

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1159,6 +1159,7 @@ pref("javascript.options.strict.debug",     false);
 #endif
 pref("javascript.options.baselinejit",      true);
 pref("javascript.options.ion",              true);
+pref("javascript.options.showInConsole",    true);
 pref("javascript.options.asmjs",            true);
 pref("javascript.options.wasm",             false);
 pref("javascript.options.native_regexp",    true);
