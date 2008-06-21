140428c56840627b1daa406c7a15fa314d2dae15
Mike Hommey <glandium@debian.org>
2008-06-21 02:48:46 +0200

    Allow .js preference files to set locked prefs with lockPref()

--- modules/libpref/prefread.h
+++ modules/libpref/prefread.h
@@ -34,7 +34,8 @@ typedef void (*PrefReader)(void       *closure,
                            PrefValue   val,
                            PrefType    type,
                            bool        defPref,
-                           bool        stickyPref);
+                           bool        stickyPref,
+                           bool        lockPref);
 
 /* structure fields are private */
 typedef struct PrefParseState {
@@ -56,6 +57,7 @@ typedef struct PrefParseState {
     PrefType    vtype;      /* PREF_STRING,INT,BOOL          */
     bool        fdefault;   /* true if (default) pref     */
     bool        fstickydefault; /* true if (sticky) pref     */
+    bool        flock;      /* true if pref to be locked     */
 } PrefParseState;
 
 /**
