9753f449a71b3c2f3a6ff1ff117e095fc20bbd5c
Ehsan Akhgari <ehsan@mozilla.com>
2010-09-06 16:31:04 -0400

    Bug 592601 - Add "face" to the list of white-listed attributes; r=bz a=dveditz

--- content/base/src/nsContentSink.cpp
+++ content/base/src/nsContentSink.cpp
@@ -1632,6 +1632,7 @@ nsIAtom** const kDefaultAllowedAttributes [] = {
   &nsGkAtoms::dir,
   &nsGkAtoms::disabled,
   &nsGkAtoms::enctype,
+  &nsGkAtoms::face,
   &nsGkAtoms::_for,
   &nsGkAtoms::frame,
   &nsGkAtoms::headers,
