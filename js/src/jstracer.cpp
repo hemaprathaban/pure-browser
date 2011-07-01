mail-apps/gecko-dev

Bug 650874 - Fix correctness issue with JSOP_CALLGVAR, r=gal,dvander, a=LegNeato

js/src/jstracer.cpp
@@ -13910,7 +13910,7 @@ TraceRecorder::record_JSOP_CALLGVAR()
 
     jsval& v = STOBJ_GET_SLOT(globalObj, slot);
     stack(0, get(&v));
-    stack(1, INS_NULL());
+    stack(1, INS_CONSTOBJ(globalObj));
     return JSRS_CONTINUE;
 }
 

hemaprathaban/pure-browser

js/src/jstracer.cpp
@@ -10723,7 +10723,7 @@ TraceRecorder::record_JSOP_CALLGVAR()
 
     jsval& v = STOBJ_GET_SLOT(globalObj, slot);
     stack(0, get(&v));
-    stack(1, INS_CONSTPTR(NULL));
+    stack(1, INS_CONSTPTR(globalObj));
     return JSRS_CONTINUE;
 }
 