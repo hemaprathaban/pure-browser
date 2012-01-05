8e4981d4a62a12d6c98e6c363730b6dcc8056e18
Trevor Saunders <trev.saunders@gmail.com>
2012-01-04 20:54:17 -0500

    bug 693343 - a11y disabled in Gnome 3 when GNOME_ACCESSIBILITY is unset patch=mgorse, tbsaunde r=tbsaunde, karlt

--- widget/src/gtk2/nsWindow.h
+++ widget/src/gtk2/nsWindow.h
@@ -345,9 +345,6 @@ public:
 #endif
     NS_IMETHOD         ReparentNativeWidget(nsIWidget* aNewParent);
 
-#ifdef ACCESSIBILITY
-    static bool        sAccessibilityEnabled;
-#endif
 protected:
     // Helper for SetParent and ReparentNativeWidget.
     void ReparentNativeWidgetInternal(nsIWidget* aNewParent,
