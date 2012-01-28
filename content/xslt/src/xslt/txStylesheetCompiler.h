f596dffd026412b31f91d2002e1e84572b4241e1
John Schoenick <john@pointysoftware.net>
2012-01-27 23:00:07 -0800

    bug702466 - Validate stack usage in xslt compiler r=sicking, a=dveditz

--- content/xslt/src/xslt/txStylesheetCompiler.h
+++ content/xslt/src/xslt/txStylesheetCompiler.h
@@ -116,6 +116,18 @@ public:
     }
 
     // Stack functions
+    enum enumStackType
+    {
+      eElementHandler,
+      eHandlerTable,
+      eVariableItem,
+      eCopy,
+      eInstruction,
+      ePushNewContext,
+      eConditionalGoto,
+      eCheckParam,
+      ePushNullTemplateRule
+    };
     nsresult pushHandlerTable(txHandlerTable* aTable);
     void popHandlerTable();
     nsresult pushSorter(txPushNewContext* aSorter);
@@ -124,8 +136,8 @@ public:
     void popChooseGotoList();
     nsresult pushObject(TxObject* aObject);
     TxObject* popObject();
-    nsresult pushPtr(void* aPtr);
-    void* popPtr();
+    nsresult pushPtr(void* aPtr, enumStackType aType);
+    void* popPtr(enumStackType aType);
 
     // stylesheet functions
     nsresult addToplevelItem(txToplevelItem* aItem);
@@ -185,6 +197,7 @@ protected:
     PRPackedBool mDoneWithThisStylesheet;
     txStack mObjectStack;
     txStack mOtherStack;
+    nsTArray<enumStackType> mTypeStack;
 
 private:
     txInstruction** mNextInstrPtr;
