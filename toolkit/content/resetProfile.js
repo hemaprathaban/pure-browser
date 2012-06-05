e8bb905dafcf8ae6c552212607d2d8ae473494d0
Mike Hommey <mh@glandium.org>
2012-06-05 08:57:06 +0200

    Bug 756390 - Make the "Reset Firefox" feature more generic

--- toolkit/content/resetProfile.js
+++ toolkit/content/resetProfile.js
@@ -28,6 +28,58 @@ function onResetProfileLoad() {
   populateResetPane("migratedItems");
 }
 
+<<<<<<< HEAD
+=======
+/**
+ * Check if reset is supported for the currently running profile.
+ *
+ * @return boolean whether reset is supported.
+ */
+function resetSupported() {
+  let profileService = Cc["@mozilla.org/toolkit/profile-service;1"].
+                       getService(Ci.nsIToolkitProfileService);
+  let currentProfileDir = Services.dirsvc.get("ProfD", Ci.nsIFile);
+
+  // Reset is only supported for the default profile if the self-migrator used for reset exists.
+  try {
+    if (currentProfileDir.equals(profileService.selectedProfile.rootDir) &&
+        "@mozilla.org/toolkit/profile-migrator;1" in Cc) {
+      let pm = Cc["@mozilla.org/toolkit/profile-migrator;1"].createInstance(Ci.nsIProfileMigrator);
+      return ("canMigrate" in pm) && pm.canMigrate("self");
+    }
+  } catch (e) {
+    // Catch exception when there is no selected profile.
+    Cu.reportError(e);
+  }
+  return false;
+}
+
+function getMigratedData() {
+  Components.utils.import("resource:///modules/MigrationUtils.jsm");
+
+  // From migration.properties
+  const MIGRATED_TYPES = [
+    4,  // History and Bookmarks
+    16, // Passwords
+    8,  // Form History
+    2,  // Cookies
+  ];
+
+  // Loop over possible data to migrate to give the user a list of what will be preserved.
+  let dataTypes = [];
+  for (let itemID of MIGRATED_TYPES) {
+    try {
+      let typeName = MigrationUtils.getLocalizedString(itemID + "_self");
+      dataTypes.push(typeName);
+    } catch (x) {
+      // Catch exceptions when the string for a data type doesn't exist.
+      Components.utils.reportError(x);
+    }
+  }
+  return dataTypes;
+}
+
+>>>>>>> Bug 756390 - Make the "Reset Firefox" feature more generic
 function onResetProfileAccepted() {
   let retVals = window.arguments[0];
   retVals.reset = true;
