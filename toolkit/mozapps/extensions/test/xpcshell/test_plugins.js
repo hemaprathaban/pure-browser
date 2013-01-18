stephenfox/iceweasel-dev

Fix some tests using CurProcD where GreD should be used

toolkit/mozapps/extensions/test/xpcshell/test_plugins.js
@@ -18,7 +18,7 @@ function run_test() {
 
 // Finds the test plugin library
 function get_test_plugin() {
-  var plugins = Services.dirsvc.get("CurProcD", AM_Ci.nsILocalFile);
+  var plugins = Services.dirsvc.get("GreD", AM_Ci.nsILocalFile);
   plugins.append("plugins");
   do_check_true(plugins.exists());
 

hemaprathaban/pure-browser

toolkit/mozapps/extensions/test/xpcshell/test_plugins.js
@@ -18,7 +18,7 @@ function run_test() {
 
 // Finds the test plugin library
 function get_test_plugin() {
-  var plugins = Services.dirsvc.get("CurProcD", AM_Ci.nsIFile);
+  var plugins = Services.dirsvc.get("GreD", AM_Ci.nsIFile);
   plugins.append("plugins");
   do_check_true(plugins.exists());
 