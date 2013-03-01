mozilla/mozilla-central

Bug 832162 - fix jar BuildSynthetics r=taras, a=akeybl

modules/libjar/nsZipArchive.cpp
@@ -562,8 +562,11 @@ MOZ_WIN_MEM_TRY_BEGIN
 
     // Sanity check variable sizes and refuse to deal with
     // anything too big: it's likely a corrupt archive.
-    if (namelen > kMaxNameLength || buf >= endp)
+    if (namelen < 1 ||
+        namelen > kMaxNameLength ||
+        buf >= endp) {
       return NS_ERROR_FILE_CORRUPTED;
+    }
 
     nsZipItem* item = CreateZipItem();
     if (!item)
@@ -614,7 +617,7 @@ MOZ_WIN_MEM_TRY_BEGIN
   // Do this when all ziptable has scanned to prevent double entries.
   for (int i = 0; i < ZIP_TABSIZE; ++i)
   {
-    for (nsZipItem* item = mFiles[i]; item != 0; item = item->next)
+    for (nsZipItem* item = mFiles[i]; item != nullptr; item = item->next)
     {
       if (item->isSynthetic)
         continue;
@@ -626,10 +629,16 @@ MOZ_WIN_MEM_TRY_BEGIN
       //-- start just before the last char so as to not add the item
       //-- twice if it's a directory
       uint16_t namelen = item->nameLength;
+      MOZ_ASSERT(namelen > 0, "Attempt to build synthetic for zero-length entry name!");
       const char *name = item->Name();
       for (uint16_t dirlen = namelen - 1; dirlen > 0; dirlen--)
       {
         if (name[dirlen-1] != '/')
+          continue;
+
+        // The character before this is '/', so if this is also '/' then we
+        // have an empty path component. Skip it.
+        if (name[dirlen] == '/')
           continue;
 
         // Is the directory already in the file table?

hemaprathaban/pure-browser

modules/libjar/nsZipArchive.cpp
@@ -642,8 +642,11 @@ MOZ_WIN_MEM_TRY_BEGIN
 
     // Sanity check variable sizes and refuse to deal with
     // anything too big: it's likely a corrupt archive.
-    if (namelen > kMaxNameLength || buf >= endp)
+    if (namelen < 1 ||
+        namelen > kMaxNameLength ||
+        buf >= endp) {
       return NS_ERROR_FILE_CORRUPTED;
+    }
 
     nsZipItem* item = CreateZipItem();
     if (!item)
@@ -682,7 +685,7 @@ MOZ_WIN_MEM_TRY_BEGIN
   // Do this when all ziptable has scanned to prevent double entries.
   for (int i = 0; i < ZIP_TABSIZE; ++i)
   {
-    for (nsZipItem* item = mFiles[i]; item != 0; item = item->next)
+    for (nsZipItem* item = mFiles[i]; item != nsnull; item = item->next)
     {
       if (item->isSynthetic)
         continue;
@@ -694,10 +697,16 @@ MOZ_WIN_MEM_TRY_BEGIN
       //-- start just before the last char so as to not add the item
       //-- twice if it's a directory
       PRUint16 namelen = item->nameLength;
+      MOZ_ASSERT(namelen > 0, "Attempt to build synthetic for zero-length entry name!");
       const char *name = item->Name();
       for (PRUint16 dirlen = namelen - 1; dirlen > 0; dirlen--)
       {
         if (name[dirlen-1] != '/')
+          continue;
+
+        // The character before this is '/', so if this is also '/' then we
+        // have an empty path component. Skip it.
+        if (name[dirlen] == '/')
           continue;
 
         // Is the directory already in the file table?