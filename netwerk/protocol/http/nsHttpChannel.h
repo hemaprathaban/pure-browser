mozilla/mozilla-central

bug 796475 - proxy auth cancelation rendering fix r=jduell a=akeybl

netwerk/protocol/http/nsHttpChannel.h
@@ -323,6 +323,7 @@ private:
     uint32_t                          mCachedContentIsPartial   : 1;
     uint32_t                          mTransactionReplaced      : 1;
     uint32_t                          mAuthRetryPending         : 1;
+    uint32_t                          mProxyAuthPending         : 1;
     uint32_t                          mResuming                 : 1;
     uint32_t                          mInitedCacheEntry         : 1;
     // True if we are loading a fallback cache entry from the

hemaprathaban/pure-browser

netwerk/protocol/http/nsHttpChannel.h
@@ -344,6 +344,7 @@ private:
     // True if mRequestTime has been set. In such a case it is safe to update
     // the cache entry's expiration time. Otherwise, it is not(see bug 567360).
     PRUint32                          mRequestTimeInitialized : 1;
+    PRUint32                          mProxyAuthPending         : 1;
 
     nsTArray<nsContinueRedirectionFunc> mRedirectFuncStack;
 