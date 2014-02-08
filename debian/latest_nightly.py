02f93bd0a14f0e2eb961ce5c1d713f860f7a3152
Mike Hommey <mh@glandium.org>
2014-02-08 10:47:52 +0900

    Use ftplib instead of ftputil in latest_nightly.py

--- debian/latest_nightly.py
+++ debian/latest_nightly.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
 import sys
-import ftputil
+from time import strptime, gmtime
+from ftplib import FTP
 from operator import itemgetter
+
+year = gmtime().tm_year
+
+def get_info(line):
+    mode, links, owner, group, size, month, day, time, file = line.split()
+    date = '%s %s %s' % (month, day, time)
+    if ':' in time:
+        return file, strptime('%s %d' % (date, year), '%b %d %H:%M %Y')
+    else:
+        return file, strptime(date, '%b %d %Y')
 
 def main():
     url = sys.argv[1]
     if not url.startswith('ftp://'):
         print >>sys.stderr, 'Expecting an ftp:// url as argument'
     (scheme, _, host, path) = url.split('/', 3)
-    host = ftputil.FTPHost(host, 'Anonymous', '')
+    ftp = FTP(host)
+    ftp.login()
     # Get the most recent .txt file in that ftp directory.
-    file = sorted([(f, host.lstat(host.path.join(path, f)).st_mtime) for f in host.listdir(path) if f.endswith('.txt')], key=itemgetter(1), reverse=True)[0][0]
-    with host.open(host.path.join(path, file)) as f:
-        (date, url) = [l.strip() for l in f.readlines()]
-    print date, url
+    ftp.cwd(path)
+    lines = []
+    ftp.retrlines('LIST', lines.append)
+    files = [get_info(l) for l in lines]
+    files = sorted([(t, f) for f, t in files if f.endswith('.txt')])
+    lines = []
+    ftp.retrlines('RETR %s' % files[-1][1], lines.append)
+    print ' '.join(lines)
 
 if __name__ == '__main__':
     main()
