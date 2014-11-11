4e5b4ee84a914ea88e4e85ee1d1fd88ea3b69b45
Mike Hommey <mh@glandium.org>
2014-11-11 12:01:49 +0900

    Set browser.aboutHomeSnippets.updateUrl to nothing

--- debian/branding/firefox-branding.js
+++ debian/branding/firefox-branding.js
@@ -1,4 +1,5 @@
 lockPref("browser.startup.homepage_override.mstone", "ignore");
+pref("browser.aboutHomeSnippets.updateUrl", "");
 pref("startup.homepage_override_url","");
 pref("startup.homepage_welcome_url","");
 pref("app.releaseNotesURL", "http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/releasenotes/");
