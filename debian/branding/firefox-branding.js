c2facc93e303e810267805951372b2fc654c3bd4
Mike Hommey <mh@glandium.org>
2014-11-11 12:00:30 +0900

    Set browser.startup.homepage_override.mstone to "ignore"

--- debian/branding/firefox-branding.js
+++ debian/branding/firefox-branding.js
@@ -1,3 +1,4 @@
+lockPref("browser.startup.homepage_override.mstone", "ignore");
 pref("startup.homepage_override_url","");
 pref("startup.homepage_welcome_url","");
 pref("app.releaseNotesURL", "http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/releasenotes/");
