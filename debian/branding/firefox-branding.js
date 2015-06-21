372ca3bdf9ad88c451a31d9549ed1d8bc51011c9
Mike Hommey <mh@glandium.org>
2015-06-22 06:29:24 +0900

    Add devtools.selfxss.count pref to the iceweasel branding to match unofficial branding
    Closes: #787975

--- debian/branding/firefox-branding.js
+++ debian/branding/firefox-branding.js
@@ -3,3 +3,4 @@ pref("browser.aboutHomeSnippets.updateUrl", "data:text/html,");
 pref("startup.homepage_override_url","");
 pref("startup.homepage_welcome_url","");
 pref("app.releaseNotesURL", "http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/releasenotes/");
+pref("devtools.selfxss.count", 0);
