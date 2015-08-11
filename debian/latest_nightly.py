326289b9de75864b2d58e68e6215d2b65f27cf73
Mike Hommey <mh@glandium.org>
2015-08-12 06:26:39 +0900

    Modify latest_nightly.py to work without ftp now that it's gone

--- debian/latest_nightly.py
+++ debian/latest_nightly.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python
 
 import sys
-from time import strptime, gmtime
-from ftplib import FTP
-from operator import itemgetter
+import httplib
+import urllib2
 
-year = gmtime().tm_year
-
-def get_info(line):
-    mode, links, owner, group, size, month, day, time, file = line.split()
-    date = '%s %s %s' % (month, day, time)
-    if ':' in time:
-        return file, strptime('%s %d' % (date, year), '%b %d %H:%M %Y')
-    else:
-        return file, strptime(date, '%b %d %Y')
 
 def main():
-    url = sys.argv[1]
-    if not url.startswith('ftp://'):
-        print >>sys.stderr, 'Expecting an ftp:// url as argument'
-    (scheme, _, host, path) = url.split('/', 3)
-    ftp = FTP(host)
-    ftp.login()
-    # Get the most recent .txt file in that ftp directory.
-    ftp.cwd(path)
-    lines = []
-    ftp.retrlines('LIST', lines.append)
-    files = [get_info(l) for l in lines]
-    files = sorted([(t, f) for f, t in files if f.endswith('.txt')])
-    lines = []
-    ftp.retrlines('RETR %s' % files[-1][1], lines.append)
-    print ' '.join(lines)
+    product = sys.argv[1]
+
+    conn = httplib.HTTPSConnection('download.mozilla.org')
+    conn.request('HEAD', '/?product=%s-latest&os=linux&lang=en-US' % product)
+    res = conn.getresponse()
+    assert res.status == 302
+    location = res.getheader('Location')
+
+    url = location.replace('.tar.bz2', '.txt')
+    assert url != location
+
+    f = urllib2.urlopen(url)
+    print ' '.join(l.rstrip() for l in f.readlines())
+    f.close()
 
 if __name__ == '__main__':
     main()
