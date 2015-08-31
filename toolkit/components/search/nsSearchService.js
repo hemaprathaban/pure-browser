085bd57ddc2b679a72884ea0669b88619a039016
Mike Hommey <mh@glandium.org>
2015-08-31 09:42:17 +0900

    Revert "Handle transition to /etc/<appname>/searchplugins more gracefully"
    This reverts commit 357a5e3557debb4b389550ab275a61c0fc6677ac.

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -1157,7 +1157,6 @@ function getMozParamPref(prefName)
  *
  * @see nsIBrowserSearchService.idl
  */
-let gForceCacheInvalidation = false;
 let gInitialized = false;
 function notifyAction(aEngine, aVerb) {
   if (gInitialized) {
@@ -2663,20 +2662,6 @@ Engine.prototype = {
    **/
   _initWithJSON: function SRCH_ENG__initWithJSON(aJson) {
     this.__id = aJson._id;
-    try {
-      let file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-      file.initWithPath(this.__id);
-      let loadDirs = [];
-      let locations = getDir(NS_APP_SEARCH_DIR_LIST, Ci.nsISimpleEnumerator);
-      while (locations.hasMoreElements()) {
-        let dir = locations.getNext().QueryInterface(Ci.nsIFile);
-        loadDirs.push(dir);
-      }
-      if (loadDirs.some(function (element) element.equals(file.parent))) {
-        this.__id = "[app]/" + file.leafName;
-        gForceCacheInvalidation = true;
-      }
-    } catch (e) { }
     this._name = aJson._name;
     this._description = aJson.description;
     if (aJson._hasPreferredIcon == undefined)
@@ -3091,19 +3076,8 @@ Engine.prototype = {
         this.__installLocation = SEARCH_APP_DIR;
       else if (this._file.parent.equals(getDir(NS_APP_USER_SEARCH_DIR)))
         this.__installLocation = SEARCH_PROFILE_DIR;
-      else {
-        let loadDirs = [];
-        let locations = getDir(NS_APP_SEARCH_DIR_LIST, Ci.nsISimpleEnumerator);
-        while (locations.hasMoreElements()) {
-          let dir = locations.getNext().QueryInterface(Ci.nsIFile);
-          loadDirs.push(dir);
-        }
-        let parent = this._file.parent;
-        if (loadDirs.some(function (element) !element.equals(parent)))
-          this.__installLocation = SEARCH_APP_DIR;
-        else
-          this.__installLocation = SEARCH_IN_EXTENSION;
-      }
+      else
+        this.__installLocation = SEARCH_IN_EXTENSION;
     }
 
     return this.__installLocation;
@@ -3468,8 +3442,6 @@ function SearchService() {
     LOG = DO_LOG;
 
   this._initObservers = Promise.defer();
-  if (gForceCacheInvalidation)
-    this._batchCacheInvalidation();
 }
 
 SearchService.prototype = {
